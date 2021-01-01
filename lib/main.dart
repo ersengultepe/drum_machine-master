0843fa261ef4c4777718096baf58748041b1d976
ersen gultepe <ersengultepe@hotmail.com>
2021-01-01 14:46:53 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DrumMachine());
+}
+
+class DrumMachine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black54,
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: <Widget>[
+                voiceButton('bip', Colors.deepOrange),
+                SizedBox(
+                  width: 10,
+                ),
+                voiceButton('bongo', Colors.blueAccent),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: <Widget>[
+                voiceButton('clap1', Colors.greenAccent),
+                SizedBox(
+                  width: 10,
+                ),
+                voiceButton('crash', Colors.yellowAccent),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: <Widget>[
+                voiceButton('clap2', Colors.purpleAccent),
+                SizedBox(
+                  width: 10,
+                ),
+                voiceButton('how', Colors.brown),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: <Widget>[
+                voiceImage('clap3', Colors.black54),
+                // voiceButton('clap3', Colors.pink),
+                SizedBox(
+                  width: 10,
+                ),
+                voiceButton('oobah', Colors.lime),
+              ],
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+AudioPlayer audioPlayer;
+AudioCache audioCache = AudioCache();
+
+playLocal(String voice) async {
+  try {
+    audioPlayer = await audioCache.play(voice + ".wav"); //audio play function
+  } catch (Exception) {
+    print(Exception.toString());
+  }
+}
+
+stopAudio() async {
+  if (audioPlayer != null) {
+    await audioPlayer.stop();
+  }
+}
+
+Expanded voiceButton(String voice, Color color) {
+  return Expanded(
+    child: FlatButton(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(34.0),
+          side: BorderSide(color: Colors.black45)),
+      height: 150,
+      minWidth: 180,
+      onPressed: () {
+        stopAudio();
+        playLocal(voice);
+      },
+      child: Text(
+        voice.toUpperCase(),
+      ),
+      color: color,
+    ),
+  );
+}
+
+Expanded voiceImage(String voice, Color color) {
+  return Expanded(
+    child: FlatButton(
+      height: 150,
+      minWidth: 180,
+      onPressed: () {
+        stopAudio();
+        playLocal(voice);
+      },
+      child: Image.asset(
+        "assets/chick.png",
+        height: 130,
+      ),
+      color: color,
+    ),
+  );
+}
